c0c9f2714a5367e7a2f11303ccbfcf231e8911b8
Liam Atkinson <lpa24@cam.ac.uk>
2016-07-08 13:31:17 +0100

    Updating circumsphere/circle calcs to return centre.

--- depth_utils/alpha_shape.py
+++ depth_utils/alpha_shape.py
@@ -1,6 +1,8 @@
 from scipy.spatial import Delaunay, ConvexHull
 import numpy as np
 from itertools import combinations
+from collections import defaultdict
+
 
 def alpha_shape(points, alpha):
   """
@@ -16,7 +18,8 @@ def alpha_shape(points, alpha):
   # simplices, but get 3 dimentional ones. So, we must convert.
   tri = Delaunay(points, qhull_options="QJ Pp")
   coords = tri.points
-  ch = tri.convex_hull
+
+  ch_lookup = SuperSimplexLookup(tri.convex_hull)
 
   # Values of a and b for each simplex.
   simplex_a = {}
@@ -25,44 +28,70 @@ def alpha_shape(points, alpha):
   for simplex in tri.simplices:
     simp_set = frozenset(simplex)
     sigma = circumsphere(coords[simplex])
-    simplex_a[simp_set] = None
-    simplex_b[simp_set] = (sigma, np.inf)
-  print("done")
+    simplex_a[simp_set] = sigma
+    simplex_b[simp_set] = sigma
 
-  # Need lookup from simplex to ALL higher simplicies.
+  sslu = SuperSimplexLookup(tri.simplices)
+  for simplex in sslu.simplices:
+    super_simplices = {s
+                       for s in sslu.lookup[simplex]
+                       if len(s) > len(simplex)}
+    a_vals = set()
+    for super_simplex in super_simplices:
+      a = simplex_a[super_simplex]
+      a_vals.add(a)
+      # b = simplex_b[super_simplex]
+    simplex_a[simplex] = min(a_vals)
 
-  # print(tri.simplices.shape)
-  simplices = _2d_simplices(tri.simplices)
-  for simplex in simplices:
-    print(simplex)
-    for x in simplex:
-      idx = (tri.vertex_to_simplex[x])
-      print(x, idx, tri.simplices[idx])
+    # try:
+    #   ch_lookup.lookup[simplex]
+    # except KeyError:
+    #   super_simplices = {s
+    #                      for s in super_simplices
+    #                      if len(s) == len(tri.convex_hull[0])}
+    #   simplex_b[simplex] = max({simplex_b[x] for x in super_simplices})
+    #   pass
+    # else:
+    #   simplex_b[simlpex] = np.inf
 
-    break
-  print(len(simplices))
+  print(len(sslu.simplices), simplex_a)
 
 
-def _2d_simplices(qhull_simplices):
-  """
-  Each qhull simplex (in R^3) is a length 4 list of indices.
-  We want to convert this into the unique list of length 3 vertices.
-  """
-  simplices = set()
-  for simplex in qhull_simplices:
-    for x in combinations(simplex, 3):
-      simplices.add(frozenset(x))
-    for x in combinations(simplex, 2):
-      simplices.add(frozenset(x))
+class SuperSimplexLookup(object):
+  def __init__(self, d_simplices):
+    """
+    Given a list of d_simplicies, create all k simplicies (k < d),
+    and a lookup from a simplex to all supersimplicies.
+    """
+    _simplices, self.lookup = self.k_simplices(d_simplices)
+    # Need to sort as we want largest K at the start,
+    # since the algorithm relies on this fact.
+    d = len(d_simplices[0])
+    self.simplices = sorted(
+        filter(lambda x: len(x) < d, _simplices),
+        key=lambda x: len(x),
+        reverse=True)
 
-  return(simplices)
+  def k_simplices(self, d_simplices):
+    """
+    Each qhull simplex (in R^3) is a length 4 list of indices.
+    We want to convert this into the unique list of length 3 vertices.
+    """
+    lookup = defaultdict(lambda: set())
+    simplices = set()
+    d = len(d_simplices[0])
+    for simplex in d_simplices:
+      simplex_key = frozenset(simplex)
+      for k in range(d, 1, -1):
+        for x in combinations(simplex, k):
+          frozen_subsimplex = frozenset(x)
+          # Add lookup simplex -> supersimplex.
+          lookup[frozen_subsimplex].add(simplex_key)
+          # Add subsimplex to list of simplices.
+          simplices.add(frozen_subsimplex)
 
-def filter_simplicies(simplices, alpha):
-  """
-  Given a list of 2-simplices (aka 3 points in R^3), filter those whose circumsphere is empty
-  for radius alpha.
-  """
-  pass
+    return (simplices, lookup)
+
 
 def circumcircle(coords):
   """
@@ -72,15 +101,21 @@ def circumcircle(coords):
   a = A - C
   b = B - C
 
+  def mag(x):
+    return (np.sqrt(x.dot(x)))
+
   ab_diff = a - b
 
   ab_cross = np.cross(a, b)
-  norm_ab_cross = np.sqrt(ab_cross.dot(ab_cross))
+  norm_ab_cross = mag(ab_cross)
 
-  r = (np.sqrt(a.dot(a)) *
-       np.sqrt(b.dot(b)) *
-       np.sqrt(ab_diff.dot(ab_diff)))  / (2 * norm_ab_cross)
-  return(r)
+  r = (mag(a) * mag(b) * mag(ab_diff)) / (2 * norm_ab_cross)
+
+  centre = (np.cross(
+      np.square(mag(a)) * b - np.square(mag(b)) * a, ab_cross) /
+            (2 * np.square(norm_ab_cross))) + C
+
+  return (centre, r)
 
 
 def circumsphere(coords):
@@ -93,17 +128,27 @@ def circumsphere(coords):
   ones = np.ones(4, dtype=np.float64)
 
   a = np.linalg.det(np.concatenate([coords, ones[:, np.newaxis]], axis=1))
-  Dx = np.linalg.det(np.stack([sqsum, coords[:, 1], coords[:, 2], ones], axis=1))
-  Dy = -1 * np.linalg.det(np.stack([sqsum, coords[:, 0], coords[:, 2], ones], axis=1))
-  Dz = np.linalg.det(np.stack([sqsum, coords[:, 0], coords[:, 1], ones], axis=1))
+  Dx = np.linalg.det(np.stack(
+      [sqsum, coords[:, 1], coords[:, 2], ones],
+      axis=1))
+  Dy = -1 * np.linalg.det(np.stack(
+      [sqsum, coords[:, 0], coords[:, 2], ones],
+      axis=1))
+  Dz = np.linalg.det(np.stack(
+      [sqsum, coords[:, 0], coords[:, 1], ones],
+      axis=1))
   c = np.linalg.det(np.concatenate([sqsum[:, np.newaxis], coords], axis=1))
 
   r = np.sqrt(Dx**2 + Dy**2 + Dz**2 - 4 * a * c) / (2 * abs(a))
 
-  return(r)
+  denom = d * a
+  centre = np.array([Dx / denom, Dy / denom, Dz / denom])
+
+  return (centre, r)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
   np.random.seed(42)
-  points = np.random.random((1000, 3))
-  alpha_shape(points, 0.5)
+  points = np.random.random((3, 3))
+  # alpha_shape(points, 0.5)
+  print(circumcircle(points))
