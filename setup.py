c07adf20b978b8747bf9342cb852c4d10681c591
latkins <liamatkinson@gmail.com>
2017-02-10 13:07:12 +0000

    Updating setup to use scripts.

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from distutils.core import setup
 
 setup(name='NucFrames',
       version='0.1',
-      package_dir={'NucFrames': 'src'},
-      packages=['NucFrames', 'NucFrames.depth_utils', 'NucFrames.distance_utils']
+      packages=['NucFrames'],
+      scripts=["scripts/make-frames"],
       )
